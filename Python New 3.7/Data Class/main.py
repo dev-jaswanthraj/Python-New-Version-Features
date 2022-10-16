2de6ccb6da65f7b8581fd4e472239425a28410ad
Jaswanthraj R <jaswanthraj.r@gmail.com>
2022-10-16 12:32:41 +0530

    3.7 - Data Class

---
+++ Python New 3.7/Data Class/main.py
@@ -0,0 +1,27 @@
+import random 
+import string
+from dataclasses import dataclass, field
+
+def generate_id() -> str:
+    return "".join(random.choices(string.ascii_uppercase, k = 12))
+
+@dataclass
+class Person:
+    name : str
+    address : str
+    active : str = True
+    email_addresses: list[str] = field(default_factory = list)
+    id : str = field(init = False, default_factory = generate_id)
+    _search_string:str = field(init = False, repr=False)
+
+    def __post_init__(self):
+        self._search_string = f"{self.name} {self.address}"
+
+    
+
+def main() -> None:
+    person = Person(name="Jaswanthraj", address="265-B Mullai Nagar", active=False)
+    print(person)
+
+if __name__ == "__main__":
+    main()
